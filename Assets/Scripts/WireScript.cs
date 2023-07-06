94217f08f37532d835b8ac3cf5f4a6f55c5908b5
Charlie Mao <118494216+maocharliemao@users.noreply.github.com>
2023-07-06 14:47:04 +1000

    i did it, i made the 5th mini game,
    basically made among us connect the wires, wot can i say

--- Assets/Scripts/WireScript.cs
+++ Assets/Scripts/WireScript.cs
@@ -7,9 +7,9 @@ public class WireScript : MonoBehaviour
     private LineRenderer Line;
     [SerializeField] private string destinationTag;
 
-    Vector3 offset;
+    private Vector3 offset;
 
-    private void start()
+    private void Start()
     {
         Line = GetComponent<LineRenderer>();
     }
@@ -19,7 +19,7 @@ public class WireScript : MonoBehaviour
         offset = transform.postion - MouseWorldPostition();
     }
 
-    private void OnMouseDrag() 
+    private void OnMouseDrag()
     {
         line.SetPosition(0, MouseWorldPosition() + offset);
         line.SetPosition(1, TransformPosition);
@@ -28,29 +28,36 @@ public class WireScript : MonoBehaviour
     private void OnMouseUp()
     {
         Vector3 rayOrigin = Camera.main.transform.position;
-        Vector3 RayDir = MouseWorldPosition()-Camera.main.transform.forward;
+        Vector3 rayDirection = MouseWorldPosition() - Camera.main.transform.position;
         RaycastHit hitInfo;
 
+<<<<<<< Updated upstream
         if(Physics.Raycast(rayorigin,RayDir, out hitInfo)) 
         { 
             if(hitInfo.transform.tag == destinationTag) 
             {
             Line.SetPosition(0,hitInfo.transform.position);
                 transform.gameObject.GetComponent<Collider>.enabled = false;
+=======
+        if (Physics.Raycast(rayOrigin, rayDirection, out hitInfo))
+        {
+            if (hitInfo.transform.CompareTag(destinationTag))
+            {
+                Line.SetPosition(0, hitInfo.transform.position);
+                transform.gameObject.GetComponent<Collider>().enabled = false;
+>>>>>>> Stashed changes
             }
             else
             {
-                Line.SetPosition(0, hitInfo.transform.position);
+                Line.SetPosition(0, hitInfo.point);
             }
         }
     }
-    
+
     private Vector3 MouseWorldPosition()
     {
         Vector3 mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Camera.main.WorldToScreenPoint.(transform.position).z;
         return Camera.main.ScreenToWorldPoint(mouseScreenPos);
     }
-
-
 }
