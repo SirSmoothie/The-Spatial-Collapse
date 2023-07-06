d17870e34861c3754a516ef21c75f9858009b0f3
Rory Rouillon <Thegameredbaron@gmail.com>
2023-07-06 13:21:04 +1000

    Commit for no reason
    Nothing really changed, helping charlie with his code

---
+++ Assets/Scripts/CuttingPath.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingPath : MonoBehaviour
+{
+
+    public float speed 1.0f;
+
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
