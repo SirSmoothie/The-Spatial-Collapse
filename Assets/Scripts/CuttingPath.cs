f90dafc219169cc4faf90908cac2606df447b95d
Rory Rouillon <Thegameredbaron@gmail.com>
2023-07-06 16:49:47 +1000

    Taking a break
    Taking a break

--- Assets/Scripts/CuttingPath.cs
+++ Assets/Scripts/CuttingPath.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CuttingPath : MonoBehaviour
 {
 
-    public float speed 1.0f;
+    public float speed = 1.0f;
+    Vector3 Offset;
 
     public 
     // Start is called before the first frame update
     void Start()
     {
-        if()
+        
     }
 
     // Update is called once per frame
