ded2d2bd1b439c2ebadf3dc4e65d9cb93154b6bc
Femurbreak <flynngoold@gmail.com>
2023-07-10 14:55:57 +1000

    Updated Game 4
    Enemy targets would only be removed once, changed a line to fix it

--- Assets/Scripts/MiniGame4/Game4Controller.cs
+++ Assets/Scripts/MiniGame4/Game4Controller.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Game4Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] myObjects;
 
-    // Update is called once per frame
     void Update()
     {
         
