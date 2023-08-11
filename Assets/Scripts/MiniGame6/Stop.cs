7f3c8830464e488570e96d36b01dddfdd97462d8
Rory Rouillon <118494211+SirSmoothie@users.noreply.github.com>
2023-08-11 15:56:58 +1000

    Minigame things

--- Assets/Scripts/MiniGame6/Stop.cs
+++ Assets/Scripts/MiniGame6/Stop.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Stop : MonoBehaviour
 {
-    public MinigameManager minigameManager;
+    public MinigameManager minigameManagerScript;
+    public GameObject MinigameManager;
+
+    private void Start()
+    {
+        if(minigameManagerScript == null)
+        {
+            MinigameManager = GameObject.Find("MinigameManager");
+            MinigameManager.GetComponent<MinigameManager>();
+            minigameManagerScript = MinigameManager.GetComponent<MinigameManager>();
+        }
+    }
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.name == "Player")
         {
             collider.gameObject.GetComponent<Character>().start = false;
-            minigameManager.NextMinigame(true);
+            minigameManagerScript.NextMinigame(true);
         }
     }
 }
