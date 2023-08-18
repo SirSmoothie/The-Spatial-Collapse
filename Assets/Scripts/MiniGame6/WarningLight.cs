76a27785e2e45084a3a0e8d143deea94beda4faf
Rory Rouillon <118494211+SirSmoothie@users.noreply.github.com>
2023-08-18 16:17:54 +1000

    Updating Minigame 6 and changed the main menu screen

---
+++ Assets/Scripts/MiniGame6/WarningLight.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningLight : MonoBehaviour
+{
+
+    public void LightsOn()
+    {
+        gameObject.SetActive(false);
+    }
+}
