adafb7da73df9c43530c9acfa5d0e4a194ef2fc9
Rory Rouillon <Thegameredbaron@gmail.com>
2023-08-22 13:49:40 +1000

    4 Line code
    Its so cursed

---
+++ Assets/ConstantRotate.cs
@@ -0,0 +1,4 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ConstantRotate : MonoBehaviour { public Vector3 TransformRotation; void FixedUpdate() { transform.Rotate(TransformRotation); } }
